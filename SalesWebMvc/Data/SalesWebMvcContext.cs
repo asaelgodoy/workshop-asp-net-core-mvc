3c3e6c88e0fbbf4a43f12480a1718dc2f3f693cd
Asael Godoy <asael.godoy00@gmail.com>
2022-12-03 22:11:12 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -9,11 +9,15 @@ namespace SalesWebMvc.Data
 {
     public class SalesWebMvcContext : DbContext
     {
-        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+
+        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecords { get; set; }
+        public DbSet<SalesWebMvc.Models.Seller> Sellers { get; set; }
+
     }
 }

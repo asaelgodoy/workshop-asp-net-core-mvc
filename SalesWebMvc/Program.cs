07a4f691926e9b4b3cf4cf33b42d07b681cd9e82
Asael Godoy <asael.godoy00@gmail.com>
2022-12-20 13:36:59 -0300

    Location service and validation of data in forms

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
 
@@ -16,6 +17,21 @@ builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
 
+// 1. 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+// 2. 
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization
+    (LanguageViewLocationExpanderFormat.SubFolder)
+    .AddDataAnnotationsLocalization();
+
+// 3. 
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    var supportedCultures = new[] { "en-US"};
+    options.SetDefaultCulture(supportedCultures[0])
+        .AddSupportedCultures(supportedCultures)
+        .AddSupportedUICultures(supportedCultures);
+});
 
 var app = builder.Build();
 
@@ -25,12 +41,32 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
+
 
 SeedDatabase();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
+
+// 4. 
+var supportedCultures = new[] { "en-US"};
+// 5. 
+// Culture from the HttpRequest
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+
+
+
+
 
 app.UseRouting();
 
